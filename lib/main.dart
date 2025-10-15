1d41c987b64c5330f4ec1bffcb523188ac4b65ef
Andy <andymean8@gmail.com>
2025-10-14 20:34:04 -0400

    Deleted base comments

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart'; // Ensure this import is present and correct
 
 void main() {
   runApp(const MyApp());
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter Demo Home Page!'),
     );
   }
 }
