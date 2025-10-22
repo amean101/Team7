eee59798ac3f18443d4d14257b98aff1ec29050d
Andy <andymean8@gmail.com>
2025-10-21 20:52:40 -0400

    added test case to see if footer buttons work and updated that in main as well

--- lib/main.dart
+++ lib/main.dart
@@ -16,55 +16,153 @@ class TraceItApp extends StatelessWidget {
         '/lostItem': (_) => const LostItemScreen(),
         '/foundItem': (_) => const FoundItemScreen(),
         '/map': (_) => const MapScreen(),
-        '/chat': (_) => const ChatScreen(),
-        '/adminHome': (_) => const AdminHomeScreen(),
-        '/adminDashboard': (_) => const AdminDashboardScreen(),
-        '/adminSearch': (_) => const AdminSearchScreen(),
+        //'/chat': (_) => const ChatScreen(),
+        //'/adminHome': (_) => const AdminHomeScreen(),
+        //'/adminDashboard': (_) => const AdminDashboardScreen(),
+        //'/adminSearch': (_) => const AdminSearchScreen(),
       },
     );
   }
 }
 
-class SignInScreen extends StatelessWidget {
-  const SignInScreen({super.key});
+class _FooterIconButton extends StatelessWidget {
+  final IconData icon;
+  final VoidCallback onPressed;
+  final bool isSelected;
+
+  const _FooterIconButton({
+    required this.icon,
+    required this.onPressed,
+    this.isSelected = false,
+  });
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: AppBar(title: Text('Sign In')),
-      body: SizedBox.shrink(),
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        IconButton(
+          icon: Icon(icon),
+          iconSize: 28,
+          color: Colors.black,
+          onPressed: onPressed,
+        ),
+        if (isSelected)
+          Container(
+            margin: const EdgeInsets.only(top: 4),
+            width: 40,
+            height: 3,
+            decoration: BoxDecoration(
+              color: Colors.black,
+              borderRadius: BorderRadius.circular(2),
+            ),
+          ),
+      ],
     );
   }
 }
 
-class LostItemScreen extends StatelessWidget {
-  const LostItemScreen({super.key});
+//** Sign In Screen **
+
+class SignInScreen extends StatelessWidget {
+  const SignInScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: AppBar(title: Text('Report Lost Item')),
-      body: SizedBox.shrink(),
+    return Scaffold(
+      appBar: AppBar(title: const Text('Sign In')),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () => Navigator.pushNamed(context, '/home'),
+          child: const Text('Go to Home'),
+        ),
+      ),
     );
   }
 }
+
+//** Home Screen **
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Home')),
+      body: Center(child: Column()),
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.1),
+              blurRadius: 8,
+              offset: const Offset(0, -2),
+            ),
+          ],
+        ),
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 12),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                _FooterIconButton(
+                  icon: Icons.explore_outlined,
+                  onPressed: () {
+                    // ** Add navigation functionality to Map Screen **
+                  },
+                ),
+                _FooterIconButton(
+                  icon: Icons.home,
+                  isSelected: true,
+                  onPressed: () {
+                    // ** Home Screen is already selected **
+                  },
+                ),
+                _FooterIconButton(
+                  icon: Icons.chat_bubble_outline,
+                  onPressed: () {
+                    // Add navigation functionality to Chat Screen **
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+//** Lost Item Screen **
+
+class LostItemScreen extends StatelessWidget {
+  const LostItemScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(body: SizedBox.shrink());
+  }
+}
+
+//** Found Item Screen **
 
 class FoundItemScreen extends StatelessWidget {
   const FoundItemScreen({super.key});
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: AppBar(title: Text('Report Found Item')),
-      body: SizedBox.shrink(),
-    );
+    return const Scaffold(body: SizedBox.shrink());
   }
 }
+
+//** Map Screen **
 
 class MapScreen extends StatelessWidget {
   const MapScreen({super.key});
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: AppBar(title: Text('Map')),
-      body: SizedBox.shrink(),
-    );
+    return const Scaffold(body: SizedBox.shrink());
   }
 }
