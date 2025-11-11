4a71a50abccfdfbda3a0b43699e618a42c03485f
Afton Goulding <aftografto@gmail.com>
2025-11-11 13:44:13 -0500

    staging

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,8 +8,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:traceit/main.dart';
-
 void main() {
   // Build our app and trigger a frame.
 
