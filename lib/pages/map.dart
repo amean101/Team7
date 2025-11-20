ce0d6e37145b7dc40b6c77e2288d308a50558e96
Aditya <adibasel04@gmail.com>
2025-11-20 15:52:32 -0500

    prepping for merge

--- lib/pages/map.dart
+++ lib/pages/map.dart
@@ -1,7 +1,33 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const MapScreen(),
+    );
+  }
+}
+
 class MapScreen extends StatelessWidget {
   const MapScreen({super.key});
+
   @override
-  Widget build(BuildContext context) => const Scaffold(body: SizedBox.shrink());
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          "Map Screen Loaded!",
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
 }
--- test/unit_test.dart
+++ test/unit_test.dart
@@ -1,8 +1,4 @@
 import 'package:flutter_test/flutter_test.dart';
-<<<<<<< HEAD
-import 'package:traceit/main.dart';
-=======
->>>>>>> origin/afton
 import 'package:flutter/material.dart';
 
 class MyTestWidget extends StatelessWidget {
@@ -32,8 +28,37 @@ void main() {
 
     expect(find.byIcon(Icons.add), findsOneWidget);
   });
-<<<<<<< HEAD
 }
-=======
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:traceit/main.dart';
+import 'package:flutter/material.dart';
+
+class MyTestWidget extends StatelessWidget {
+  const MyTestWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Icon Test')),
+      body: Center(
+        child: IconButton(
+          icon: const Icon(
+            Icons.add,
+          ), // Testing to see if the icon button works
+          onPressed: () {},
+        ),
+      ),
+    );
+  }
 }
->>>>>>> origin/afton
+
+void main() {
+  testWidgets('NavigationButtons Test', (WidgetTester tester) async {
+    await tester.pumpWidget(const MaterialApp(home: MyTestWidget()));
+
+    await tester.pumpAndSettle();
+
+    expect(find.byIcon(Icons.add), findsOneWidget);
+  });
+}
